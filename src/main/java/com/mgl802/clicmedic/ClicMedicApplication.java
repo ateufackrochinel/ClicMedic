fbcc3e295029f57bca7c471bfa88d5a9b4eff9b8
rochinel ateufack <rochinelateufack0@gmail.com>
2024-06-10 11:18:37 -0400

    creation de l'environnement Backend

---
+++ src/main/java/com/mgl802/clicmedic/ClicMedicApplication.java
@@ -0,0 +1,14 @@
+package com.mgl802.clicmedic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClicMedicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClicMedicApplication.class, args);
+		System.out.println("bonjour");
+	}
+
+}
---
+++ src/test/java/com/mgl802/clicmedic/ClicMedicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mgl802.clicmedic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClicMedicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
