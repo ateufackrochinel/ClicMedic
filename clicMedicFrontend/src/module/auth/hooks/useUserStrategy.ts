6ab5a5c8e51448a2241f48339ad737363f89f123
guytchoupo <guytchoupo@gmail.com>
2024-07-08 22:22:04 -0400

    polish strategy

--- clicMedicFrontend/src/module/auth/hooks/useUserStrategy.ts
+++ clicMedicFrontend/src/module/auth/hooks/useUserStrategy.ts
@@ -1,26 +1,15 @@
 import {
-  GetMedecin,
-  GetPatient,
+  ConcreteUser,
   UserStrategy,
 } from '@clicMedic/model/user-strategy/strategy';
 import { SignUpType } from '../types';
 
 export const useUserStrategy = (values: SignUpType) => {
-  let newValues = {} as SignUpType;
-
-  if (values.type === 'patient') {
-    const patientStrategy: UserStrategy = new UserStrategy(
-      new GetPatient(),
-      values
-    );
-    newValues = patientStrategy.executeStrategy();
-  } else if (values.type === 'medecin') {
-    const medecinStrategy: UserStrategy = new UserStrategy(
-      new GetMedecin(),
-      values
-    );
-    newValues = medecinStrategy.executeStrategy();
-  }
+  const userstrategy: UserStrategy = new UserStrategy(
+    new ConcreteUser(),
+    values
+  );
+  const newValues: SignUpType = userstrategy.executeStrategy();
 
   return {
     newValues,
